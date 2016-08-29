f0716c4c30b04ddfb3e054496ddeb5d614229d0e
Manjunadh Bommisetty <manjunadhb@manjunadhs-macbook-pro.local>
2016-08-29 17:17:59 +0530

    Initial Project has been created and uploading to repository

---
+++ BRNPulseBy1604/BRNPulseBy1604/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BRNPulseBy1604
+//
+//  Created by Manjunadh Bommisetty on 29/08/16.
+//  Copyright © 2016 BRN Infotech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BRNPulseBy1604/BRNPulseBy1604/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BRNPulseBy1604
+//
+//  Created by Manjunadh Bommisetty on 29/08/16.
+//  Copyright © 2016 BRN Infotech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
